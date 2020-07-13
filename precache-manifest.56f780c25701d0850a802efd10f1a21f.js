025e135d55fd0ba0b323719fb41d7c8d67a7fb05
aleksey-10 <alexcare1337@gmail.com>
2020-07-13 12:22:48 +0300

    Updates

---
+++ precache-manifest.56f780c25701d0850a802efd10f1a21f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21642c01df531df7c0fd54934754b321",
+    "url": "/ultra-todo-app/index.html"
+  },
+  {
+    "revision": "f6f26fcb5038f36d5d3f",
+    "url": "/ultra-todo-app/static/css/main.492246c3.chunk.css"
+  },
+  {
+    "revision": "54dbd10b8ba249d8a16b",
+    "url": "/ultra-todo-app/static/js/2.63a4dabb.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/ultra-todo-app/static/js/2.63a4dabb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6f26fcb5038f36d5d3f",
+    "url": "/ultra-todo-app/static/js/main.18e3d4ab.chunk.js"
+  },
+  {
+    "revision": "5b50f81daf7eeffc0c14",
+    "url": "/ultra-todo-app/static/js/runtime-main.2fd1ae7b.js"
+  }
+]);
